6f4b2257fd63119c433eaaac9181851e2d3871d2
Izak Burger <isburger@gmail.com>
2020-01-15 12:47:24 +0200

    v0.5

--- dbus_characterdisplay.py
+++ dbus_characterdisplay.py
@@ -19,7 +19,7 @@ from pages import LanPage, WlanPage, VebusErrorPage, SolarErrorPage, VebusAlarms
 from four_button_ui import FourButtonUserInterface
 from simple_ui import SimpleUserInterface
 
-VERSION = 0.4
+VERSION = 0.5
 FOUR_BUTTON_DEVICES = ['victronenergy,paygo']
 
 # Set up i18n
